b5f05a93738587c8d37d3d66a4d38409eda86c81
Ale Weber <alexis.weber@mailfence.com>
2024-05-23 15:03:07 -0300

    first steps on RUST

---
+++ rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-02c9b6e3b9075186.d
@@ -0,0 +1,5 @@
+/home/ale/Projects/Projects/Rust/rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-02c9b6e3b9075186: src/main.rs
+
+/home/ale/Projects/Projects/Rust/rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-02c9b6e3b9075186.d: src/main.rs
+
+src/main.rs:
---
+++ rust-learning-path/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/ale/Projects/Projects/Rust/rust-learning-path/hello-cargo/target/debug/hello-cargo: /home/ale/Projects/Projects/Rust/rust-learning-path/hello-cargo/src/main.rs
