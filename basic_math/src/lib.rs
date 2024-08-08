4e743d2799a330cccfed68ebe8bd3f168657aad8
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-08 16:55:33 -0300

    test de documetacion

---
+++ basic_math/src/lib.rs
@@ -0,0 +1,18 @@
+/// Generalmente, la primera línea es un breve resumen que describe la función.
+///
+/// Las siguientes líneas presentan documentación detallada.
+/// Los bloques de código comienzan con ticks triples. El código tiene un implícito `fn main()` inside and `extern crate <cratename>`,
+/// lo que significa que puedes empezar a escribir código.
+///
+/// ```
+/// let result = basic_math::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+    
+    
+}
+
+
+
